73d80501a0d2c17cab92450511e3ac3f954ea506
Seth Hoenig <seth.a.hoenig@gmail.com>
2012-04-06 22:50:01 -0500

    looks for `$HOME/.gistitrc` file, which is pointless, because it will not be supported until later

--- gistit.go
+++ gistit.go
@@ -1,7 +1,51 @@
 package main
 
+import "errors"
 import "fmt"
+import "os"
+import "path/filepath"
+import "strings"
 
 func main() {
-	fmt.Printf("Hello, World\n")
+	fmt.Printf("gistit\n")
+	err := gistitrc()
+	if err == nil {
+		fmt.Println("Using .gistitrc")
+	} else {
+		fmt.Println("Not using .gistitrc")
+	}
+}
+
+func gistitrc() error {
+	env := os.Environ()
+	homedir, err := getHOMEfromEnv(env)
+	if err == nil {
+		gistitrcPath := filepath.Join(homedir, ".gistitrc")
+		file, err := os.Open(gistitrcPath)
+		if err != nil {
+			// `~/.gistitrc` does not exit, not that it matters
+			return errors.New("No gistitrc file found")
+		} else {
+			fmt.Println("~/.gistitrc Found, but not supported")
+			defer file.Close()
+			return nil // someday update return values to get needed auth
+		}
+		fmt.Println(gistitrcPath)
+	}
+	fmt.Printf("No $HOME was found")
+	return nil
+}
+
+func getHOMEfromEnv(env []string) (string, error) {
+	homePath := ""
+	for i := range env {
+		if strings.HasPrefix(env[i], "HOME=") {
+			homePath = env[i][5:]
+			break
+		}
+	}
+	if homePath == "" {
+		return "", errors.New("no gistrc file was found")
+	}
+	return homePath, nil
 }
