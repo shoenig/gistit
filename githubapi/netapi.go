4083733b8e11dd350dd76a10c72b9dd26b9caae7
Seth Hoenig <seth.a.hoenig@gmail.com>
2012-04-09 19:54:21 -0500

    add netapi.ListUserGists

--- githubapi/netapi.go
+++ githubapi/netapi.go
@@ -1,6 +1,7 @@
 package githubapi
 
 import "strconv"
+import "encoding/json"
 import "net/http"
 import "io/ioutil"
 import "fmt"
@@ -15,4 +16,21 @@ func GetGistResponse(id int) GistResponse {
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 	return jsonToGistResponse(body)
+}
+
+func ListUserGists(user string) []GistResponse {
+	resp, httperr := http.Get(api_url + "users/" + user + "/gists")
+	if httperr != nil {
+		fmt.Println("Error connecting to api.github.com", httperr)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	var gr []GistResponse
+	jsonerr := json.Unmarshal(body, &gr)
+	if jsonerr != nil {
+		fmt.Printf("Error unmarshaling JSON content", jsonerr)
+		os.Exit(1)
+	}
+	return gr
 }
