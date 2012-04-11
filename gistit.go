8ba042f31b0cb1024298826c6850ca07ae2f3044
Seth Hoenig <seth.a.hoenig@gmail.com>
2012-04-11 16:02:59 -0500

    move some helper funcs into tools

--- gistit.go
+++ gistit.go
@@ -1,10 +1,7 @@
 package main
 
-import "errors"
 import "fmt"
 import "os"
-import "path/filepath"
-import "strings"
 import "./githubapi"
 
 func main() {
@@ -30,38 +27,4 @@ func main() {
 		fmt.Println(gr)
 		fmt.Println(gr.Html_Url)
 	}
-}
-
-func gistitrc() error {
-	env := os.Environ()
-	homedir, err := getHOMEfromEnv(env)
-	if err == nil {
-		gistitrcPath := filepath.Join(homedir, ".gistitrc")
-		file, err := os.Open(gistitrcPath)
-		if err != nil {
-			// `~/.gistitrc` does not exit, not that it matters
-			return errors.New("No gistitrc file found")
-		} else {
-			fmt.Println("~/.gistitrc Found, but not supported")
-			defer file.Close()
-			return nil // someday update return values to get needed auth
-		}
-		fmt.Println(gistitrcPath)
-	}
-	fmt.Printf("No $HOME was found")
-	return nil
-}
-
-func getHOMEfromEnv(env []string) (string, error) {
-	homePath := ""
-	for i := range env {
-		if strings.HasPrefix(env[i], "HOME=") {
-			homePath = env[i][5:]
-			break
-		}
-	}
-	if homePath == "" {
-		return "", errors.New("no gistrc file was found")
-	}
-	return homePath, nil
 }
--- tools.go
+++ tools.go
@@ -5,6 +5,8 @@ import "fmt"
 import "strings"
 import "os"
 import "flag"
+import "errors"
+import "path/filepath"
 import "./githubapi"
 
 func setFlags() (help bool, description, filename string, args []string) {
@@ -55,4 +57,38 @@ func readFiles(filenames []string) ([]githubapi.File, error) {
 		files = append(files, githubapi.NewFile(f, string(c)))
 	}
 	return files, nil
+}
+
+func gistitrc() error {
+	env := os.Environ()
+	homedir, err := getHOMEfromEnv(env)
+	if err == nil {
+		gistitrcPath := filepath.Join(homedir, ".gistitrc")
+		file, err := os.Open(gistitrcPath)
+		if err != nil {
+			// `~/.gistitrc` does not exit, not that it matters
+			return errors.New("No gistitrc file found")
+		} else {
+			fmt.Println("~/.gistitrc Found, but not supported")
+			defer file.Close()
+			return nil // someday update return values to get needed auth
+		}
+		fmt.Println(gistitrcPath)
+	}
+	fmt.Printf("No $HOME was found")
+	return nil
+}
+
+func getHOMEfromEnv(env []string) (string, error) {
+	homePath := ""
+	for i := range env {
+		if strings.HasPrefix(env[i], "HOME=") {
+			homePath = env[i][5:]
+			break
+		}
+	}
+	if homePath == "" {
+		return "", errors.New("no gistrc file was found")
+	}
+	return homePath, nil
 }
